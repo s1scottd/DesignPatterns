a4855b388d094d9981a9ce41652dfb9a1aa7186d
s1scottd <scottdavis.gv@gmail.com>
2022-09-08 15:11:31 -0700

    Work through Section 3 of course.

---
+++ DesignPatterns.Creational.Factories.Exercise2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DesignPatterns.Creational.Factories.Exercise2
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class PersonFactory
+	{
+		private static int _id;
+
+		public Person CreatePerson(string name)
+		{
+			return new Person()
+			{
+				Name = name,
+				Id = _id++
+			};
+		}
+	}
+
+
+	internal class Program
+	{
+		static void Main()
+		{
+			PersonFactory factory = new PersonFactory();
+			Person fred = factory.CreatePerson("Fred");
+			Person sally = factory.CreatePerson("Sally");
+
+			Console.WriteLine($"{fred.Name} {fred.Id}");
+			Console.WriteLine($"{sally.Name} {sally.Id}");
+		}
+	}
+}
