75d344c860f6ba1feaff3366f787ba97fcfe4a27
s1scottd <scottdavis.gv@gmail.com>
2022-09-09 15:44:58 -0700

    Work through Section 3 of course.

---
+++ DesignPatterns.Creational.Prototype.Exercise3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static System.Console;
+
+namespace DesignPatterns.Creational.Prototype.Exercise3
+{
+	public class Point
+	{
+		public int X, Y;
+
+		public override string ToString()
+		{
+			return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+		}
+	}
+
+	public class Line
+	{
+		public Point Start, End;
+
+		public Line DeepCopy()
+		{
+			return new Line
+			{
+				Start = new Point()
+				{
+					X = this.Start.X,
+					Y = this.Start.Y
+				},
+				End = new Point()
+				{
+					X = this.End.X,
+					Y = this.End.Y
+				}
+			};
+		}
+
+		public override string ToString()
+		{
+			return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}";
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var line1 = new Line
+			{
+				Start = new Point()
+				{
+					X = 0,
+					Y = 0
+				},
+				End = new Point()
+				{
+					X =	1,
+					Y = 1
+				}
+			};
+
+			var line2 = line1.DeepCopy();
+			line2.Start.X = 10;
+
+			WriteLine($"line1: {line1}");
+			WriteLine($"line2: {line2}");
+		}
+	}
+}
---
+++ DesignPatterns.Creational.Prototype.ICloneable/Program.cs
@@ -0,0 +1,70 @@
+﻿using static System.Console;
+
+namespace DesignPatterns.Creational.Prototype.ICloneable
+{
+	// ICloneable is ill-specified
+
+	public class Address : System.ICloneable
+	{
+		public readonly string StreetName;
+		public int HouseNumber;
+
+		public Address(string streetName, int houseNumber)
+		{
+			StreetName = streetName;
+			HouseNumber = houseNumber;
+		}
+
+		public override string ToString()
+		{
+			return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+		}
+
+		public object Clone()
+		{
+			return new Address(StreetName, HouseNumber);
+		}
+	}
+
+	public class Person : System.ICloneable
+	{
+		public readonly string[] Names;
+		public readonly Address Address;
+
+		public Person(string[] names, Address address)
+		{
+			Names = names;
+			Address = address;
+		}
+
+		public override string ToString()
+		{
+			return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+		}
+
+		public object Clone()
+		{
+			return new Person(Names, Address);
+		}
+	}
+
+	public static class Demo
+	{
+		static void Main()
+		{
+			var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+
+			var jane = (Person)john.Clone();
+			jane.Address.HouseNumber = 321; // oops, John is now at 321
+
+			// this doesn't work
+			//var jane = john;
+
+			// but clone is typically shallow copy
+			jane.Names[0] = "Jane";
+
+			WriteLine(john);
+			WriteLine(jane);
+		}
+	}
+}
